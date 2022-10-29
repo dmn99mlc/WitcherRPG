3ae7a8b2e59025c800224cf0238d1bdcdff350d4
dmn99mlc <116599043+dmn99mlc@users.noreply.github.com>
2022-10-29 13:43:28 +0200

    Poprawa przypadkowych ataków, zmiana obsługi potencjału agresora

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -13,20 +13,19 @@ public class Calculator {
         do {
             System.out.println("Potencjał Agresora: ");
             int attackerPotential = scan.nextInt();
-            System.out.println("Potencjał Obrońcy: ");
-            int DEFPotential = scan.nextInt();
 
-            System.out.println("Wybierz typ ataku: \n 1 - Zwykły cios \n 2 - Szybki cios \n 3 - Silny cios");
+
+            System.out.println("Wybierz typ ataku: \n 1 - Zwykły cios \n 2 - Szybki cios \n 3 - Silny cios\n 4 - Atak dystansowy");
             int actionType = scan.nextInt();
             if (actionType == 1){               // Zwykły cios
-                AttackTest.attack(attackerPotential, DEFPotential, actionType);
+                meleeAttack.attack(attackerPotential, actionType);
             } else if (actionType == 2){        // Szybki cios
-                AttackTest.attack(attackerPotential, DEFPotential, actionType);
-                System.out.println("Potencjał Obrońcy: ");
-                DEFPotential = scan.nextInt();  // Silny cios
-                AttackTest.attack(attackerPotential, DEFPotential, actionType);
-            } else if (actionType == 3){
-                AttackTest.attack(attackerPotential - 3,DEFPotential, actionType);
+                meleeAttack.attack(attackerPotential, actionType);
+                meleeAttack.attack(attackerPotential, actionType);
+            } else if (actionType == 3){        // Silny cios
+                meleeAttack.attack(attackerPotential, actionType);
+            } else if (actionType == 4){        // Atak dystansowy
+                rangedAttack.attack(attackerPotential);
             }
         } while (true);
 
--- src/Calculator/meleeAttack.java
+++ src/Calculator/meleeAttack.java
@@ -2,9 +2,9 @@ package Calculator;
 
 import java.util.Scanner;
 
-class AttackTest {
+class meleeAttack {
 
- public static void attack(int attackerPotential, int DEFPotential, int actionType) {
+ public static void attack(int attackerPotential, int actionType) {
 
   int ATTresult, DEFresult;
   float locMultiplier;
@@ -12,6 +12,13 @@ class AttackTest {
   int measuringMod = 0;
 
   Scanner scan = new Scanner(System.in);
+
+
+
+
+  if (actionType == 3){
+   attackerPotential = attackerPotential - 3;
+  }
 
   System.out.println("Czy atak jest mierzony?\n1 - Tak\n0 - Nie");
   int measured = scan.nextInt();
@@ -35,12 +42,15 @@ class AttackTest {
    int ATTFumbleThrow = scan.nextInt();
    locMultiplier = 3;
    locationName = "Głowa";
-   Fumble.ATTFumble(ATTFumbleThrow, actionType, locationName);
+   Fumble.ATTFumble(ATTFumbleThrow);
    ATTresult = attackerPotential + measuringMod - ATTFumbleThrow;
   } else{
    ATTresult = attackerRoll + attackerPotential + measuringMod;
   }
 
+
+  System.out.println("Potencjał Obrońcy: ");
+  int DEFPotential = scan.nextInt();
 
 
   System.out.println("Rzut Obrońcy: ");
@@ -50,7 +60,7 @@ class AttackTest {
   if (DEFroll ==1){
    System.out.println("Rzuć na pech (1d10)");
    int DEFFumbleThrow = scan.nextInt();
-   Fumble.DEFFumble(DEFFumbleThrow, actionType, locationName);
+   Fumble.DEFFumble(DEFFumbleThrow);
    DEFresult = DEFPotential - DEFFumbleThrow;
   }
 
